93a1432263a594dd9c26567aa573a5f9fd2e455f
Aurélien Stromboni <aurelien.stromboni@outlook.com>
2021-04-17 12:45:07 +0200

    Rangement

--- Photosorter.Windows/FileMover.cs
+++ Photosorter.Windows/FileMover.cs
@@ -1,10 +1,6 @@
 ﻿using PhotoSorter.Core;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhotoSorter.Windows
 {
